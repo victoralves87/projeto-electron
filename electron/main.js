502dfab5d99dd5346521535b92d0e97c04a1fcb8
victor alves <voctorralves@gmail.com>
2024-05-22 18:59:28 -0300

    ajeitando botao excluir

--- electron/main.js
+++ electron/main.js
@@ -9,60 +9,59 @@ const musicDir = path.join(__dirname,"..","public","musicas")
 function createWindow() {
     // Cria a janela do navegador.
     mainWindow = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        nodeIntegration: true, // Permite a integração do Node.js no front-end
-        preload: `${__dirname}/preload.js`
-      },
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true, // Permite a integração do Node.js no front-end
+            preload: `${__dirname}/preload.js`
+        },
     });
-      // Carrega o arquivo index.html da aplicação.
-     mainWindow.loadURL('http://localhost:3000');
-     mainWindow.on('closed', function () {
+    // Carrega o arquivo index.html da aplicação.
+    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.on('closed', function () {
         mainWindow = null;
-      });
+    });
 }
 
-app.whenReady().then(async () =>{
+app.whenReady().then(async () => {
     createWindow();
-})
+});
 
-app.on('window-all-cloesed', () =>{
-    if(process.platform !== 'darwin'){
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
-app.on('activate', () =>{
-    if(mainWindow === null){
+app.on('activate', () => {
+    if (mainWindow === null) {
         createWindow();
     }
 });
 
 ipcMain.on("music-upload", (event, file) => {
-  const filePath = path.join(musicDir, file.name);
-  fs.writeFile(filePath, file.data, async (err) => {
-      if (err) {
-          mainWindow.webContents.send("toast:receive", err);
-      } else {
-         sendUpdateList()
-          mainWindow.webContents.send("toast:receive", "Arquivo recebido com sucesso");
-      }
-  });
+    const filePath = path.join(musicDir, file.name);
+    fs.writeFile(filePath, file.data, async (err) => {
+        if (err) {
+            mainWindow.webContents.send("toast:receive", err);
+        } else {
+            sendUpdateList(); // Corrigido aqui
+            mainWindow.webContents.send("toast:receive", "Arquivo recebido com sucesso");
+        }
+    });
 });
 
-ipcMain.on("music-get", ()=>{
+ipcMain.on("music-get", () => {
     sendUpdateList();
-})
+});
 
 ipcMain.on("remove-listener", (event, channel) => {
     mainWindow.webContents.send("remove-listener", channel);
 });
 
-
-async function sendUpdateList(){
-    const files = await fs.promises.readdir(musicDir)
-    mainWindow.webContents.send("music-list", files)
+async function sendUpdateList() {
+    const files = await fs.promises.readdir(musicDir);
+    mainWindow.webContents.send("music-list", files);
 }
 
 ipcMain.on("music-delete", async (event, file) => {
@@ -71,7 +70,7 @@ ipcMain.on("music-delete", async (event, file) => {
         if (err) {
             mainWindow.webContents.send("toast:receive", err);
         } else {
-            sendUpdatedList();
+            sendUpdateList(); // Corrigido aqui
             mainWindow.webContents.send("toast:receive", "Arquivo foi deletado com sucesso");
         }
     });
@@ -80,4 +79,3 @@ ipcMain.on("music-delete", async (event, file) => {
 ipcMain.on("music-to-play", (event, file) => {
     mainWindow.webContents.send("music-playable", file);
 });
-
